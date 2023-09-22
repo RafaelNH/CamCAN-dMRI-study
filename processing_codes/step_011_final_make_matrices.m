0ff1970ec58b526c15bace4e6e2ec51a3a539c8c
Rafael Neto Henriques <rafaelnh21@gmail.com>
2023-09-22 16:39:55 +0100

    Update and rename step_010_final_make_matrices.m to step_011_final_make_matrices.m

--- processing_codes/step_011_final_make_matrices.m
+++ processing_codes/step_011_final_make_matrices.m
@@ -1,5 +1,5 @@
 % Rafael Neto Henriques, 17/03/2023
-addpath('NIFTI_toolbox/')
+addpath('/home/rh04/DKIu_v1.1/NIFTI_toolbox/')
 
 load DWIlist.mat
 
@@ -32,6 +32,12 @@ Len = length(subject);
 % Initialzie matrix
 FA_wm_mat=zeros(Len, 48);
 FA_wm=zeros(Len, 1);
+MD_wm_mat=zeros(Len, 48);
+MD_wm=zeros(Len, 1);
+AD_wm_mat=zeros(Len, 48);
+AD_wm=zeros(Len, 1);
+RD_wm_mat=zeros(Len, 48);
+RD_wm=zeros(Len, 1);
 MKdf_wm_mat=zeros(Len, 48);
 MKdf_wm=zeros(Len, 1);
 MDdf_wm_mat=zeros(Len, 48);
@@ -42,6 +48,12 @@ ND_wm_mat=zeros(Len, 48);
 ND_wm=zeros(Len, 1);
 Fiso_wm_mat=zeros(Len, 48);
 Fiso_wm = zeros(Len, 1);
+MK_wm_mat=zeros(Len, 48);
+MK_wm=zeros(Len, 1);
+AK_wm_mat=zeros(Len, 48);
+AK_wm=zeros(Len, 1);
+RK_wm_mat=zeros(Len, 48);
+RK_wm=zeros(Len, 1);
 
 nvoxel_wm=zeros(Len, 1);
 nvoxel_wm_mat=zeros(Len, 48);
@@ -54,6 +66,12 @@ for s=1:Len
     % Load diffusion metrics
     V=load_untouch_nii([pth, subject(s).name, '/dti/FA.nii']);
     FA=V.img;
+    V=load_untouch_nii([pth, subject(s).name, '/dti/MD.nii']);
+    MD=V.img;
+    V=load_untouch_nii([pth, subject(s).name, '/dti/AD.nii']);
+    AD=V.img;
+    V=load_untouch_nii([pth, subject(s).name, '/dti/RD.nii']);
+    RD=V.img;
     V=load_untouch_nii([pth, subject(s).name, '/dkidf/MD.nii']);
     MDdf=V.img;
     V=load_untouch_nii([pth, subject(s).name, '/dkidf/MK.nii']);
@@ -66,6 +84,12 @@ for s=1:Len
     ND=V.img;
     V=load_untouch_nii([pth, subject(s).name, '/NODDI_fiso.nii']);
     Fiso=V.img;
+    V=load_untouch_nii([pth, subject(s).name, '/dki/MK.nii']);
+    MK=V.img;
+    V=load_untouch_nii([pth, subject(s).name, '/dki/AK.nii']);
+    AK=V.img;
+    V=load_untouch_nii([pth, subject(s).name, '/dki/RK.nii']);
+    RK=V.img;
     
     if NODDIth
         WM(Fiso>fwth)=0;
@@ -78,11 +102,17 @@ for s=1:Len
     
     nvoxel_wm(s) = sum(WM(:)>th);
     FA_wm(s) = mean(FA(WM > th));
+    MD_wm(s) = mean(MD(WM > th));
+    AD_wm(s) = mean(AD(WM > th));
+    RD_wm(s) = mean(RD(WM > th));
     MKdf_wm(s) = mean(MKdf(WM > th));
     MDdf_wm(s) = mean(MDdf(WM > th));
     OD_wm(s) = mean(OD(WM > th));
     ND_wm(s) = mean(ND(WM > th));
     Fiso_wm(s) = mean(Fiso(WM > th));
+    MK_wm(s) = median(MK(WM > th));
+    AK_wm(s) = median(AK(WM > th));
+    RK_wm(s) = median(RK(WM > th));
     
     
     for r=1:48
@@ -91,18 +121,27 @@ for s=1:Len
             
             nvoxel_wm_mat(s, r) = sum(WM(:)== r);
             FA_wm_mat(s, r) = mean(FA(WM == r));
+            MD_wm_mat(s, r) = mean(MD(WM == r));
+            AD_wm_mat(s, r) = mean(AD(WM == r));
+            RD_wm_mat(s, r) = mean(RD(WM == r));
             MKdf_wm_mat(s, r) = mean(MKdf(WM == r));
             MDdf_wm_mat(s, r) = mean(MDdf(WM == r));
             OD_wm_mat(s, r) = mean(OD(WM == r));
             ND_wm_mat(s, r) = mean(ND(WM == r));
             Fiso_wm_mat(s, r) = mean(Fiso(WM == r));
+            MK_wm_mat(s, r) = median(MK(WM == r));
+            AK_wm_mat(s, r) = median(AK(WM == r));
+            RK_wm_mat(s, r) = median(RK(WM == r));
 
         end
     end
     disp(s)
 end
 
-save('Diffusion_vs_Age_WMlabels_mean_noddi90', 'subject', 'sex', 'age',...
-    'FA_wm','MKdf_wm','MDdf_wm','OD_wm','ND_wm','Fiso_wm',...
-    'FA_wm_mat','MKdf_wm_mat','MDdf_wm_mat','OD_wm_mat','ND_wm_mat','Fiso_wm_mat',...
+save('Diffusion_vs_Age_WMlabels_mean_noddi90_expanded', 'subject', 'sex', 'age',...
+    'FA_wm','MD_wm','AD_wm','RD_wm','MKdf_wm','MDdf_wm','OD_wm','ND_wm','Fiso_wm',...
+    'MK_wm','AK_wm','RK_wm',...
+    'FA_wm_mat','MD_wm_mat','AD_wm_mat','RD_wm_mat',...
+    'MKdf_wm_mat','MDdf_wm_mat','OD_wm_mat','ND_wm_mat','Fiso_wm_mat',...
+    'MK_wm_mat','AK_wm_mat','RK_wm_mat',...
     'nvoxel_wm','nvoxel_wm_mat')
